3c5fbc23418b6d904dc62150715c86a3a31e3a57
liueleven <liueleven@aliyun.com>
2019-09-09 17:53:37 +0800

    修改端口测试配置中心

--- eurekaclient/src/main/java/cn/eleven/springcloud/controller/HelloController.java
+++ eurekaclient/src/main/java/cn/eleven/springcloud/controller/HelloController.java
@@ -1,15 +1,17 @@
 package cn.eleven.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @description: 一定要写注释啊
+ * @description: Spring Boot 2.0 以后，actuator 默认只开启 health 和 info 端点，要使用 refresh 端点需要通过 management 指定。
  * @date: 2019-09-09 15:50
  * @author: 十一
  */
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
